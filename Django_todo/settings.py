4defa778354b684058ee090f5092d27637109ca4
Emelie Markkanen Åberg <emeliemarkkanen@hotmail.com>
2023-02-19 10:03:46 +0000

    Finalize db, heroku, github setup in settings.py and env.py

--- Django_todo/settings.py
+++ Django_todo/settings.py
@@ -27,12 +27,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 
 # Application definition
@@ -82,11 +83,8 @@ WSGI_APPLICATION = 'Django_todo.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-       os.environ.get("postgres://ieuggyuh:mYFT3qC7NgjJn2YJzpcql1Y5JHpDpcmt@mouse.db.elephantsql.com/ieuggyuh")
-       }
-} 
-
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
